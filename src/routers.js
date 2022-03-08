c3e5bf6faa3dc6f2bd882f17dcb420bd1a3682f1
= <=>
2022-03-08 07:53:37 +0700

    clone and run project

--- src/routers.js
+++ src/routers.js
@@ -11,23 +11,24 @@ const browserHistory = createBrowserHistory()
 
 const PrivateRoute = (props) => {
   const [cookies] = useCookies([STORAGEKEY.ACCESS_TOKEN])
+  const Component = props.component
 
   return (
-    <Route {...props.rest} exact
-      render = {(prop) => (
-        const Component = prop.component;
+    <Route
+      {...props.rest}
+      exact
+      render={(prop) =>
         cookies[STORAGEKEY.ACCESS_TOKEN] ? (
           <Component {...prop} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { redirect_url: prop.location }
+            }}
+          />
         )
-          : (
-            <Redirect
-              to={{
-                pathname: '/login',
-                state: { redirect_url: prop.location }
-              }}
-            />
-          )
-      )}
+      }
     />
   )
 }
@@ -35,15 +36,20 @@ const PrivateRoute = (props) => {
 const WhiteListRoute = (props) => {
   const whiteList = ['/login', '/forget-password']
   const [cookies] = useCookies([STORAGEKEY.ACCESS_TOKEN])
-  const isWhiteList = (path) => !cookies[STORAGEKEY.ACCESS_TOKEN] && whiteList.indexOf(path) >= 0
+  const isWhiteList = (path) =>
+    !cookies[STORAGEKEY.ACCESS_TOKEN] && whiteList.indexOf(path) >= 0
 
   return (
-    <Route {...props.rest} exact
-      render = {(prop) => (
-        isWhiteList(props.path)
-          ? (<div>{React.createElement(props.component, prop)}</div>)
-          : (<Redirect to={{ pathname: '/' }} />)
-      )}
+    <Route
+      {...props.rest}
+      exact
+      render={(prop) =>
+        isWhiteList(props.path) ? (
+          <div>{React.createElement(props.component, prop)}</div>
+        ) : (
+          <Redirect to={{ pathname: '/' }} />
+        )
+      }
     />
   )
 }
@@ -65,10 +71,21 @@ export const appRouter = [
 
 const renderRouter = (routes) => {
   let arr = []
-  routes.forEach(route => {
-    const tmpRoute = route.meta.isPrivate
-      ? (<PrivateRoute exact path={route.path} component={route.component} key={route.name} />)
-      : (<WhiteListRoute path={route.path} component={route.component} key={route.name} />)
+  routes.forEach((route) => {
+    const tmpRoute = route.meta.isPrivate ? (
+      <PrivateRoute
+        exact
+        path={route.path}
+        component={route.component}
+        key={route.name}
+      />
+    ) : (
+      <WhiteListRoute
+        path={route.path}
+        component={route.component}
+        key={route.name}
+      />
+    )
     if (checkPermission(route.meta.role)) {
       arr.push(tmpRoute)
     }
@@ -88,8 +105,8 @@ const routes = () => {
     <div className={`main-content ${isWhiteList && 'whitelist'}`}>
       <Router history={browserHistory}>
         <Switch>
-          { renderRouter(appRouter).map(render => render) }
-          <PrivateRoute path='/test/:id' component={Keyword} />
+          {renderRouter(appRouter).map((render) => render)}
+          {/* <PrivateRoute path='/test/:id' component={Keyword} /> */}
           <Route path='*' component={NotFoundRoute} />
         </Switch>
       </Router>
