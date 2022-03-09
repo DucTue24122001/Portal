9e6f5ed7e5403fdf5caf82c400db9b848fdb9d63
Dang Trong Truong <dangtrongtruong190197@gmail.com>
2022-03-09 19:18:03 +0700

    update login/router

--- src/layouts/components/header/index.js
+++ src/layouts/components/header/index.js
@@ -1,4 +1,4 @@
-import React, { memo, useState } from 'react'
+import React, { useState } from 'react'
 import { Menu, Row, Col, Typography } from 'antd'
 import { Link } from 'react-router-dom'
 import { SettingOutlined, PoweroffOutlined } from '@ant-design/icons'
