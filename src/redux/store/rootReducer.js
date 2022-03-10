ed73f0d7565856c3c4ff8eff749043397b4d7f23
Dang Trong Truong <dangtrongtruong190197@gmail.com>
2022-03-10 10:59:15 +0700

    update login/logout api

--- src/redux/store/rootReducer.js
+++ src/redux/store/rootReducer.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux'
 import { loginReducer } from '../login'
-import { timeSheetReducer } from '../timesheet'
 
 export default combineReducers({
-  login: loginReducer,
-  timesheet: timeSheetReducer
+  login: loginReducer
 })
